035822bc79397d54b4ee6de12bb45dffb6750a95
apoio-forLEARN <apoio@forlearn.ao>
2023-10-30 16:35:31 +0100

    second commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <App>
+
+  </App>
+</template>
+<script>
+    import App from "../template/App.vue"
+    export default{
+        props: {title: String},
+        setup(){
+            let welcome = "Bem vindo"
+
+            return {
+                welcome,
+            }
+        },
+        components: {
+            App,
+        }
+    }
+</script>
---
+++ resources/js/template/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <Navbar site="Produtos"/>
+    <slot />
+</template>
+<script>
+    import Navbar from '../Components/Navbar.vue';
+    export default {
+        setup(){
+            return {
+
+            }
+        },
+        components: {
+            Navbar,
+        }
+    }
+</script>
